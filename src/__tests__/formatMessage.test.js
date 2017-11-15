c1fbd022182dec44263e637a1b1a47ef1a5959ef
Adrian Perez <adrian@adrianperez.org>
2017-11-15 18:19:46 +0100

    test: add formatMessage tests

---
+++ src/__tests__/formatMessage.test.js
@@ -0,0 +1,17 @@
+const formatMessage = require('../formatMessage');
+
+describe('formatMessage', () => {
+  it('returns a list of strings suitable for console logging', () => {
+    expect(
+      formatMessage('query', { operationName: 'FormatMessageQuery' }, 1000),
+    ).toMatchSnapshot();
+
+    expect(
+      formatMessage(
+        'mutation',
+        { operationName: 'FormatMessageMutation' },
+        500,
+      ),
+    ).toMatchSnapshot();
+  });
+});
