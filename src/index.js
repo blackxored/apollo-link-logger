ff54f947988c83ba22bab3c7491698ade51c65ab
dmt0 <dmitry@plot.ly>
2020-08-13 20:52:35 -0400

    feat: add support for apollo-link v3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved,import/extensions */
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink } from '@apollo/client';
 import formatMessage from './formatMessage';
 import logging from './logging';
 
