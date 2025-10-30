08429d12b5a6f0518ff6c03b76048243a254d9c2
Adrian Perez <adrian@adrianperez.org>
2025-10-29 19:52:56 -0700

    feat: attempt to support Apollo v4
    ref: #76
    
    BREAKING CHANGE: Only supports Apollo Client version >= 4.
    
    Thanks to @maxsalven

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 /* eslint-disable import/no-unresolved,import/extensions */
 import { ApolloLink } from '@apollo/client/core';
+import { map } from 'rxjs';
 import formatMessage from './formatMessage';
 import logging from './logging';
 
 const loggerLink = new ApolloLink((operation, forward) => {
   const startTime = new Date().getTime();
 
-  return forward(operation).map(result => {
-    const operationType = operation.query.definitions[0].operation;
-    const ellapsed = new Date().getTime() - startTime;
+  return forward(operation).pipe(
+    map(result => {
+      const { operationType } = operation;
+      const ellapsed = new Date().getTime() - startTime;
 
-    const group = formatMessage(operationType, operation, ellapsed);
+      const group = formatMessage(operationType, operation, ellapsed);
 
-    logging.groupCollapsed(...group);
+      logging.groupCollapsed(...group);
 
-    logging.log('INIT', operation);
-    logging.log('RESULT', result);
+      logging.log('INIT', operation);
+      logging.log('RESULT', result);
 
-    logging.groupEnd(...group);
-    return result;
-  });
+      logging.groupEnd(...group);
+      return result;
+    }),
+  );
 });
 
 export default loggerLink;
